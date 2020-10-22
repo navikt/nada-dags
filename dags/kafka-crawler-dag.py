79c59c10adbd2c7f2691692f92481b1056a23124
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-10-22 16:00:48 +0200

    import

--- dags/kafka-crawler-dag.py
+++ dags/kafka-crawler-dag.py
@@ -3,8 +3,6 @@ from airflow.kubernetes.volume import Volume
 from airflow.kubernetes.volume_mount import VolumeMount
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-#from airflow.contrib.kubernetes.volume import Volume
-#from airflow.contrib.kubernetes.volume_mount import VolumeMount
 from datetime import datetime, timedelta
 from kubernetes.client.models import V1Container
 
@@ -22,10 +20,6 @@ with DAG('kafka-indexer', default_args=default_args, schedule_interval=timedelta
         init_containers=[V1Container(
             name="init-clone-repo",
             image="navikt/knada-git-sync:9",
-            volume_mounts=[
-                VolumeMount("dags-data", mount_path="/dags", sub_path=None, read_only=False),
-                VolumeMount("git-clone-secret", mount_path="/keys", sub_path=None, read_only=False)
-            ],
             command=["/bin/sh", "/git-clone.sh"],
             args=["navikt/nada-dags", "main", "/dags"]
         )],
