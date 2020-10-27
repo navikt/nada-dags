ff5a22a2191d9be197a52f3282e2df77a4b77ed2
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-10-27 12:30:05 +0100

    pythonoperatordag

---
+++ dags/python-operator-dag.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+default_args = {
+    'start_date': datetime(2019, 4, 1)
+}
+
+
+def print_context():
+    df = pd.DataFrame({"test": ["1", "2", "3"]})
+    print(df)
+
+
+with DAG('eksempel', default_args=default_args, schedule_interval=timedelta(days=1)) as dag:
+    run_this = PythonOperator(
+        task_id='test',
+        python_callable=print_context,
+        dag=dag
+    )
+
+    run_this
