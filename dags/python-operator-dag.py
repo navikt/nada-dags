f5929fae1a4cedac7c532664eb70c422bbca92b5
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-10-28 10:45:38 +0100

    update

--- dags/python-operator-dag.py
+++ dags/python-operator-dag.py
@@ -5,12 +5,17 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
 
+default_args = {
+    'start_date': datetime(2020, 10, 28)
+}
+
+
 def print_context():
     df = pd.DataFrame({"test": ["1", "2", "3"]})
     print(df)
 
 
-with DAG('pythonoperatortest', start_date=datetime(2020, 10, 28), schedule_interval=None) as dag:
+with DAG('pythonoperatortest', default_args=default_args, schedule_interval=None) as dag:
     run_this = PythonOperator(
         task_id='test',
         python_callable=print_context,
