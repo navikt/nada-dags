288b27ec0bf6e8305a174668b20f005954d50987
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-10-29 15:28:23 +0100

    correct ns

--- dags/kafka-crawler-dag.py
+++ dags/kafka-crawler-dag.py
@@ -34,7 +34,7 @@ with DAG('kafka-indexer', default_args=default_args, schedule_interval=None) as 
         init_containers=[git_clone_init_container],
         dag=dag,
         name='kafka-indexer',
-        namespace='opendata',
+        namespace='nada',
         task_id='kafka-indexer',
         image='navikt/knada-airflow-nb:5',
         env_vars={
