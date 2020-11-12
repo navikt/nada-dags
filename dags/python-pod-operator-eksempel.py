d49894bfcf926f9d3953e9a0532380f09f50df96
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-12 14:49:40 +0100

    rename task

--- dags/python-pod-operator-eksempel.py
+++ dags/python-pod-operator-eksempel.py
@@ -5,7 +5,7 @@ from dataverk_airflow.knada_operators import create_knada_python_pod_operator
 
 with DAG('knada-python-pod-operator-eksempel', start_date=datetime(2020, 11, 9), schedule_interval=None) as dag:
     task = create_knada_python_pod_operator(dag=dag,
-                                            name="knada-pod-operator",
+                                            name="knada-python-pod-operator",
                                             repo="navikt/nada-dags",
                                             script_path="scripts/main.py",
                                             email="erik.vattekar@nav.no",
