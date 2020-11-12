d8c1de6e26657c35fb219353e5cbb6b2d3482610
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-12 14:40:01 +0100

    add python pod operator example

---
+++ dags/python-pod-operator-eksempel.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from dataverk_airflow.knada_operators import create_knada_python_pod_operator
+
+
+with DAG('knada-python-pod-operator-eksempel', start_date=datetime(2020, 11, 9), schedule_interval=None) as dag:
+    task = create_knada_python_pod_operator(dag=dag,
+                                            name="knada-pod-operator",
+                                            repo="navikt/nada-dags",
+                                            script_path="scripts/main.py",
+                                            email="erik.vattekar@nav.no",
+                                            slack_channel="#kubeflow-cron-alerts",
+                                            namespace="nada",
+                                            branch="main",
+                                            retries=1,
+                                            retry_delay=timedelta(seconds=5))
---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+import dataverk
+import pandas as pd
+from dataverk_vault import api as vault_api
+
+vault_api.set_secrets_as_envs()
+
+
+print("tester knada python pod operator")
