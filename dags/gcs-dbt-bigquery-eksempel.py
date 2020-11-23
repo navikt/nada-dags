a30471f846248396bd54b3cdf0af1f07689db8b8
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-23 16:38:21 +0100

    må lagres i gbq først før dbt kan brukes

--- dags/gcs-dbt-bigquery-eksempel.py
+++ dags/gcs-dbt-bigquery-eksempel.py
@@ -5,13 +5,24 @@ from airflow.utils.dates import days_ago
 
 
 with DAG('gcs-dbt-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
-    task = create_knada_nb_pod_operator(dag=dag,
-                                        name="read-ssb-write-gcs",
-                                        repo="navikt/nada-dags",
-                                        nb_path="notebooks/FetchStyrkUploadGCS.ipynb",
-                                        email="erik.vattekar@nav.no",
-                                        namespace="nada",
-                                        branch="main",
-                                        log_output=True,
-                                        retries=1,
-                                        retry_delay=timedelta(seconds=5))
+    read_ssb_store_gcs = create_knada_nb_pod_operator(dag=dag,
+                                                      name="read-ssb-write-gcs",
+                                                      repo="navikt/nada-dags",
+                                                      nb_path="notebooks/FetchStyrkUploadGCS.ipynb",
+                                                      email="erik.vattekar@nav.no",
+                                                      namespace="nada",
+                                                      branch="main",
+                                                      log_output=True,
+                                                      retries=1,
+                                                      retry_delay=timedelta(seconds=5))
+
+    read_gcs_process_store_gbq = create_knada_nb_pod_operator(dag=dag,
+                                                              name="read-gcs-process-write-gbq",
+                                                              repo="navikt/nada-dags",
+                                                              nb_path="notebooks/GcsToBigQuery.ipynb",
+                                                              email="erik.vattekar@nav.no",
+                                                              namespace="nada",
+                                                              branch="main",
+                                                              log_output=True,
+                                                              retries=1,
+                                                              retry_delay=timedelta(seconds=5))
