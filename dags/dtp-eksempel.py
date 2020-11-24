ffbf8023277b07b66c7de99d244b05accbc8fddc
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-24 14:51:31 +0100

    add dtp_run step

--- dags/dtp-eksempel.py
+++ dags/dtp-eksempel.py
@@ -1,15 +1,19 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
+from airflow_dbt import DbtRunOperator
 from dataverk_airflow.knada_operators import create_knada_dbt_seed_operator
 
 with DAG('dtp-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
     seed_gcs = create_knada_dbt_seed_operator(dag,
-                                              name="seed-gcs-bucket",
+                                              name="seed-gcs-blob",
                                               repo="navikt/nada-dags",
                                               namespace="nada",
                                               dbt_dir="styrk",
                                               seed_source={"gcs_bucket": "styrk-bucket",
                                                            "blob_name": "styrk-koder/styrk.csv"},
-                                              delete_on_finish=False,
                                               email="erik.vattekar@nav.no",
                                               branch="main")
+
+    dtp_run = DbtRunOperator(task_id='dbt-run')
+
+    seed_gcs >> dtp_run
