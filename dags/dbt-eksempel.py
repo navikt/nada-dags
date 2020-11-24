3fc49652e99de5c53d3b81d8a906a548769ca06a
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-24 15:52:33 +0100

    use cusom operator for dbt run

--- dags/dbt-eksempel.py
+++ dags/dbt-eksempel.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow_dbt import DbtRunOperator
-from dataverk_airflow.knada_operators import create_knada_dbt_seed_operator
+from dataverk_airflow.knada_operators import create_knada_dbt_seed_operator, create_knada_dbt_run_operator
 
 with DAG('dtp-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
     seed_gcs = create_knada_dbt_seed_operator(dag,
@@ -14,6 +13,12 @@ with DAG('dtp-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None
                                               email="erik.vattekar@nav.no",
                                               branch="main")
 
-    dtp_run = DbtRunOperator(task_id='dbt-run')
+    dbt_run = create_knada_dbt_run_operator(dag,
+                                            name="dbt-run",
+                                            repo="navikt/nada-dags",
+                                            namespace="nada",
+                                            dbt_dir="styrk",
+                                            email="erik.vattekar@nav.no",
+                                            branch="main")
 
-    seed_gcs >> dtp_run
+    seed_gcs >> dbt_run
