edcefbd8738bfe85f11c8817c3696e1431a24245
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-24 10:20:49 +0100

    start dbt eksempel

---
+++ dags/dtp-eksempel.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from dataverk_airflow.knada_operators import create_knada_dbt_seed_operator
+
+with DAG('dtp-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
+    seed_gcs = create_knada_dbt_seed_operator(dag,
+                                              name="seed-gcs-bucket",
+                                              repo="navikt/nada-dags",
+                                              namespace="nada",
+                                              dbt_dir="styrk",
+                                              seed_source={"bucket_name": "styrk-bucket",
+                                                           "blob_name": "styrk-kode/styrk.csv"},
+                                              email="erik.vattekar@nav.no",
+                                              branch="main")
--- dags/gcs-bigquery-eksempel.py
+++ dags/gcs-bigquery-eksempel.py
@@ -4,7 +4,7 @@ from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 from airflow.utils.dates import days_ago
 
 
-with DAG('gcs-dbt-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
+with DAG('gcs-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
     read_ssb_store_gcs = create_knada_nb_pod_operator(dag=dag,
                                                       name="read-ssb-write-gcs",
                                                       repo="navikt/nada-dags",
