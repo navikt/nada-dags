b6dff6ebff0d6baae4eb55972429ef8e8a3957d6
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-25 14:03:53 +0100

    duplicate names for dag

--- dags/dbt-s3-eksempel.py
+++ dags/dbt-s3-eksempel.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from dataverk_airflow.knada_operators import create_knada_dbt_seed_operator, create_knada_dbt_run_operator
 
-with DAG('dtp-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
+with DAG('dbt-s3-bigquery-eksempel', start_date=days_ago(0), schedule_interval=None) as dag:
     seed_s3 = create_knada_dbt_seed_operator(dag,
                                              name="seed-gcs-blob",
                                              repo="navikt/nada-dags",
