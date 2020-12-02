bb1ca5cbc9636c7494a61bca3346c707fca1902f
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-12-02 15:53:40 +0100

    test uten å spesifisere external task id

--- dags/ekstern-avhengighet.py
+++ dags/ekstern-avhengighet.py
@@ -5,10 +5,10 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.sensors import ExternalTaskSensor
 
 
-with DAG('ekstern-avhengighet', start_date=datetime(2020, 11, 9), schedule_interval="50 14 * * *", catchup=False) as dag:
+with DAG('ekstern-avhengighet', start_date=datetime(2020, 11, 9), schedule_interval="55 14 * * *", catchup=False) as dag:
     task = ExternalTaskSensor(
         external_dag_id="bash-operator-eksempel",
-        external_task_id="byetask",
+        #external_task_id="byetask",
         task_id="wait-for-external-task-completion"
     )
 
