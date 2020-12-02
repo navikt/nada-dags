f66c8c74f095ffd9ffcbd6146ea832139f47a4a0
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-12-02 14:45:23 +0100

    test ExternalTaskSensor

---
+++ dags/ekstern-avhengighet.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.sensors import ExternalTaskSensor
+from airflow.utils.dates import days_ago
+
+
+with DAG('ekstern-avhengighet', start_date=days_ago(0), schedule_interval="1/* * * * *", catchup=False) as dag:
+    task = ExternalTaskSensor(
+        external_dag_id="bash-operator-eksempel"
+    )
+
+    t2 = BashOperator(
+        task_id='test',
+        bash_command='echo "test"',
+        dag=dag
+    )
+
+    task >> t2
