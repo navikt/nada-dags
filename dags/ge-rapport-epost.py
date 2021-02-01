360fecace39e3ebbb722709936331061cd3eb67e
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-02-01 13:02:31 +0100

    revert

--- dags/ge-rapport-epost.py
+++ dags/ge-rapport-epost.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 from airflow.operators.email_operator import EmailOperator
-from airflow.operators.slack_operator import SlackAPIPostOperator
+#from airflow.operators.slack_operator import SlackAPIPostOperator
 from airflow.utils.dates import days_ago
 from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 
@@ -29,27 +29,26 @@ with DAG('ge-rapport-varsling', start_date=days_ago(1), schedule_interval=None) 
                                provide_context=True,
                                html_content="<b><h1> {{ task_instance.xcom_pull(task_ids='ge-validation') }} </h1></b>")
 
-    slack_post = SlackAPIPostOperator(
-        dag=dag,
-        username="Airflow DAG reporter",
-        task_id="slack_valideringsresultater",
-        token=os.environ["SLACK_WEBHOOK_TOKEN"],
-        text="{{ task_instance.xcom_pull(task_ids='ge-validation') }}",
-        channel="#kubeflow-cron-alerts",
-        icon_url="https://github.com/apache/airflow/raw/v1-10-stable/airflow/www/static/pin_100.png"
-    )
-
-    # slack_notification = SlackWebhookOperator(
+    # slack_post = SlackAPIPostOperator(
     #     dag=dag,
+    #     username="Airflow DAG reporter",
     #     task_id="slack_valideringsresultater",
-    #     webhook_token=os.environ["SLACK_WEBHOOK_TOKEN"],
-    #     message=f"{{ task_instance.xcom_pull(task_ids='ge-validation') }}",
+    #     token=os.environ["SLACK_WEBHOOK_TOKEN"],
+    #     text="{{ task_instance.xcom_pull(task_ids='ge-validation') }}",
     #     channel="#kubeflow-cron-alerts",
-    #     link_names=True,
-    #     icon_emoji=":page_with_curl:",
-    #     provide_context=True,
-    #     proxy=os.environ["HTTPS_PROXY"]
+    #     icon_url="https://github.com/apache/airflow/raw/v1-10-stable/airflow/www/static/pin_100.png"
     # )
 
-    ge_validering >> send_epost >> slack_post
-    #>> slack_notification
+    slack_notification = SlackWebhookOperator(
+        dag=dag,
+        task_id="slack_valideringsresultater",
+        webhook_token=os.environ["SLACK_WEBHOOK_TOKEN"],
+        message=f"{{ task_instance.xcom_pull(task_ids='ge-validation') }}",
+        channel="#kubeflow-cron-alerts",
+        link_names=True,
+        icon_emoji=":page_with_curl:",
+        provide_context=True,
+        proxy=os.environ["HTTPS_PROXY"]
+    )
+
+    ge_validering >> send_epost >> slack_notification
