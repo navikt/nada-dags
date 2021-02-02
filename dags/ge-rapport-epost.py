8c693b25383b347ff63c69723c8cf48ecd7a0150
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-02-02 10:20:30 +0100

    formater slack melding

--- dags/ge-rapport-epost.py
+++ dags/ge-rapport-epost.py
@@ -1,4 +1,5 @@
 import os
+import json
 from datetime import timedelta
 from airflow import DAG
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
@@ -16,7 +17,7 @@ with DAG('ge-rapport-varsling', start_date=days_ago(1), schedule_interval=None) 
             val_args = results[val_error]['expectation_config']['expectation_type']['kwargs']
             del val_args['column']
             del val_args['result_format']
-            val_type = results[val_error]['expectation_config']['expectation_type'] + " with args " + val_args
+            val_type = results[val_error]['expectation_config']['expectation_type'] + " with args " + json.dumps(val_args)
             status = results[val_error]['success']
             err_msg += "\n" \
 f"""    _{val_error}_:
