1e06c769076f2d77f532b7da1831e9b3f38f1668
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-02-16 10:07:31 +0100

    test

--- dags/knada-pod-operator-eksempel.py
+++ dags/knada-pod-operator-eksempel.py
@@ -5,15 +5,15 @@ from airflow.utils.dates import days_ago
 from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 
 
-with DAG('knada-pod-operator-eksempel', start_date=days_ago(1), schedule_interval="*/5 * * * *") as dag:
-    task = create_knada_nb_pod_operator(dag=dag,
-                                        name="knada-pod-operator",
-                                        repo="navikt/nada-dags",
-                                        nb_path="notebooks/Transformation.ipynb",
-                                        email="erik.vattekar@nav.no",
-                                        slack_channel="#kubeflow-cron-alerts",
-                                        namespace="nada",
-                                        branch="main",
-                                        log_output=True,
-                                        retries=1,
-                                        retry_delay=timedelta(seconds=5))
+with DAG('test', start_date=days_ago(1), schedule_interval="*/5 * * * *") as dag:
+    t1 = create_knada_nb_pod_operator(dag=dag,
+                                      email="erik.vattekar@nav.no",
+                                      name="knada-pod-operator",
+                                      slack_channel="#kubeflow-cron-alerts",
+                                      repo="navikt/nada-dags",
+                                      nb_path="notebooks/Transformation.ipynb",
+                                      namespace="nada",
+                                      branch="main",
+                                      log_output=True)
+
+    t1
