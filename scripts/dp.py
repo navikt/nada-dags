d31fa75042633c9b313c2bfa453e9ae0edf0ef79
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-02-25 14:07:37 +0100

    correct author

--- scripts/dp.py
+++ scripts/dp.py
@@ -4,7 +4,7 @@ import datetime as dt
 dp = Datapackage({
     "title": "erik tester",
     "bucket": "nav-opendata",
-    "author": {"name": "NAV Kunnskapsavdelingen", "email": "statistikk@nav.no"},
+    "author": 'statistikk@nav.no',
     "temporal": {"from": "2010", "to": "2020"},
     "issued": "2021-02-02",
     "modified": dt.datetime.now().isoformat(),
