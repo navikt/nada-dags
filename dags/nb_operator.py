f92c254d262f155b407097044b782093083aeae4
Erik Vattekar <erik.vattekar@nav.no>
2022-10-14 10:52:40 +0200

    add

--- dags/nb_operator.py
+++ dags/nb_operator.py
@@ -7,11 +7,12 @@ from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 with DAG('test-nb-operator', start_date=days_ago(1), schedule_interval=None) as dag:
     t1 = create_knada_nb_pod_operator(dag=dag,
                                       name="knada-pod-operator",
-                                      slack_channel="#kubeflow-cron-alerts"
+                                      slack_channel="#kubeflow-cron-alerts",
                                       repo="navikt/nada-dags",
                                       nb_path="notebooks/mynb.ipynb",
                                       namespace="team-nada",
                                       branch="main",
+                                      delete_on_finish=False,
                                       log_output=True)
 
     t1
