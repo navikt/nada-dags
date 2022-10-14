b71a9e49c045fc61b7b15d21da5132bf0c14e5d2
Erik Vattekar <erik.vattekar@nav.no>
2022-10-14 09:21:17 +0200

    email alerts not ready yet

--- dags/nb_operator.py
+++ dags/nb_operator.py
@@ -6,7 +6,6 @@ from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 
 with DAG('test-nb-operator', start_date=days_ago(1), schedule_interval=None) as dag:
     t1 = create_knada_nb_pod_operator(dag=dag,
-                                      email="erik.vattekar@nav.no",
                                       name="knada-pod-operator",
                                       slack_channel="#nada-alerts",
                                       repo="navikt/nada-dags",
