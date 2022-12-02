106c500e26a4924500637244c7c92a1526a77fd6
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2022-12-02 10:33:00 +0100

    Update nb_operator.py

--- dags/nb_operator.py
+++ dags/nb_operator.py
@@ -10,9 +10,10 @@ with DAG('test-nb-operator', start_date=days_ago(1), schedule_interval=None) as 
                                       slack_channel="#kubeflow-cron-alerts",
                                       repo="navikt/nada-dags",
                                       nb_path="notebooks/mynb.ipynb",
+                                      delete_on_finish=False,
                                       resources={
                                         "limit_memory": "3G"
-                                      }
+                                      },
                                       branch="main")
 
     t1
