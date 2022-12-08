03aaf8d2900a17699effa797d6c5834996319259
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2022-12-08 13:19:28 +0100

    Update nb_operator.py

--- dags/nb_operator.py
+++ dags/nb_operator.py
@@ -3,6 +3,13 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from kubernetes import client
 from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
+import os
+from google.cloud import secretmanager
+secrets = secretmanager.SecretManagerServiceClient()
+
+resource_name = f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest"
+secret = secrets.access_secret_version(name=resource_name)
+data = secret.payload.data.decode('UTF-8')
 
 
 with DAG('test-nb-operator', start_date=days_ago(1), schedule_interval=None) as dag:
