5985d14c4965b2bdae9acb84162cf7bcc0d32f04
Erik Vattekar <erik.vattekar@nav.no>
2023-01-24 14:32:16 +0100

    add simple pythonoperator dag

---
+++ dags/py.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+import os
+
+def myfunc():
+    print("func")
+    print("team secret path", os.environ["KNADA_TEAM_SECRET"])
+
+with DAG('test-k8s-exec', start_date=days_ago(1), schedule_interval=None) as dag:
+    run_this = PythonOperator(
+    task_id='test',
+    python_callable=myfunc,
+    dag=dag)
