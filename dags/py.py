54518d3d5e2c55ce07cfdf8ed4cb96d91922523d
Erik Vattekar <erik.vattekar@nav.no>
2023-01-24 15:05:38 +0100

    add logging

--- dags/py.py
+++ dags/py.py
@@ -3,10 +3,11 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 import os
+import logging
 
 def myfunc():
-    print("func")
-    print("team secret path", os.environ["KNADA_TEAM_SECRET"])
+    logging.info("func")
+    logging.warning("team secret path", os.environ["KNADA_TEAM_SECRET"])
 
 with DAG('test-k8s-exec', start_date=days_ago(1), schedule_interval=None) as dag:
     run_this = PythonOperator(
