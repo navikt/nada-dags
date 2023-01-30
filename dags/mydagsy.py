2c0c231605947f6cdff96d3051f300b9b0e64cec
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-01-30 13:34:16 +0100

    Update mydagsy.py

--- dags/mydagsy.py
+++ dags/mydagsy.py
@@ -13,7 +13,8 @@ with DAG('test-python-operator', start_date=days_ago(1), schedule_interval=None)
                                       repo="navikt/nada-dags",
                                       script_path="notebooks/script.py",
                                       resources=client.V1ResourceRequirements(
-                                          limits={"memory": "128M"}
+                                          limits={"memory": "128M"},
+                                          requests={"memory": "128M"}
                                       ),
                                       branch="main")
 
