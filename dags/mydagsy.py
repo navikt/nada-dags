8bf0f9377df540729a6d182553cd3cd350a0205b
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-02-01 08:46:00 +0100

    Update mydagsy.py

--- dags/mydagsy.py
+++ dags/mydagsy.py
@@ -5,8 +5,6 @@ from kubernetes import client
 from dataverk_airflow.knada_operators import create_knada_python_pod_operator
 import os
 
-os.environ["KNADA_PYTHON_POD_OP_IMAGE"] = "ghcr.io/navikt/knada-airflow:2022-12-15-d6ba810"
-
 with DAG('test-python-operator', start_date=days_ago(1), schedule_interval=None) as dag:
     t1 = create_knada_python_pod_operator(dag=dag,
                                       name="knada-python-operator",
@@ -16,6 +14,7 @@ with DAG('test-python-operator', start_date=days_ago(1), schedule_interval=None)
                                           limits={"memory": "2Gi", "cpu": "1"},
                                           requests={"memory": "2Gi", "cpu": "1"}
                                       ),
+                                      delete_on_finish=False,
                                       branch="main")
 
     t1
