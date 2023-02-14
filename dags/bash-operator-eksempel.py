c6d07f201457c0e621cdf3154380164c055978a5
Kyrre Havik <Kyrremann@gmail.com>
2023-02-14 08:57:56 +0100

    fix: stokket om på måned og dag

--- dags/bash-operator-eksempel.py
+++ dags/bash-operator-eksempel.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 
-with DAG('bash-operator-eksempel', start_date=datetime(2023, 13, 2), schedule_interval="*/5 * * * *", catchup=False) as dag:
+with DAG('bash-operator-eksempel', start_date=datetime(2023, 2, 14), schedule_interval="*/5 * * * *", catchup=False) as dag:
 
     os.environ["TEST"] = "hallo"
 
