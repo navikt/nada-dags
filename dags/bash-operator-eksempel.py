8b095a0049029702bb94308829dda52f290b7fe9
Kyrre Havik <Kyrremann@gmail.com>
2023-02-14 08:56:16 +0100

    fix: jobben kjører nå hvert 5 minutt

--- dags/bash-operator-eksempel.py
+++ dags/bash-operator-eksempel.py
@@ -4,16 +4,16 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 
-with DAG('bash-operator-eksempel', start_date=datetime(2020, 11, 9), schedule_interval="3 15 * * *", catchup=False) as dag:
+with DAG('bash-operator-eksempel', start_date=datetime(2023, 13, 2), schedule_interval="*/5 * * * *", catchup=False) as dag:
 
     os.environ["TEST"] = "hallo"
 
     t1 = BashOperator(
-        task_id='hellotask',
-        bash_command='date',
+        task_id='hello_task',
+        bash_command='echo "$TEST world at ${date}"',
         dag=dag)
     t2 = BashOperator(
-        task_id='byetask',
+        task_id='bye_task',
         bash_command='echo "Bye world"',
         dag=dag)
 
