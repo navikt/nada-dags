dc2b99d0ec0d47146581d97eb0b25e2630e8e94c
Kyrre Havik <kyrremann@gmail.com>
2023-02-15 17:47:00 +0100

    fix: manglet DAG-definisjonen

--- dags/k8s_resources.py
+++ dags/k8s_resources.py
@@ -1,15 +1,16 @@
+from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from kubernetes.client import models as client
 
-task = KubernetesPodOperator(
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10", "echo pwd"],
-    name="k8s_resource_example",
-    task_id="task-one",
-    get_logs=True,
-    container_resources=client.V1ResourceRequirements(
-        requests={"ephemeral-storage": "2Gi"}
-    ),
-)
-
-task >> run_this
+with DAG('k8s_resource_example',
+         schedule_interval="@daily") as dag:
+    task_1 = KubernetesPodOperator(
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10", "echo pwd"],
+        name="k8s_resource_example",
+        task_id="task-one",
+        get_logs=True,
+        container_resources=client.V1ResourceRequirements(
+            requests={"ephemeral-storage": "2Gi"}
+        ),
+    )
