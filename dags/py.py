f3709f03e8c10f5c9bc6df34cdfa577dcd9e0ad0
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-02-20 15:59:48 +0100

    Update py.py

--- dags/py.py
+++ dags/py.py
@@ -60,8 +60,8 @@ with DAG('test-k8s-exec', start_date=days_ago(1), schedule_interval=None) as dag
     then_this = KubernetesPodOperator(
         dag=dag,
         task_id="tasken",
-        cmd=["bash", "-c"],
-        args=["echo", "hello"],
+        cmds=["bash", "-c"],
+        arguments=["echo", "hello"],
         executor_config={
             "pod_override": k8s.V1Pod(
                 metadata=k8s.V1ObjectMeta(annotations={"allowlist": "data.ssb.no,dm07-scan.adeo.no:1521"})
