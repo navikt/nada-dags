d4bfe6d36a676be7aacf6b5cd7318a82faa1ab78
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-02-20 12:01:26 +0100

    Update py.py

--- dags/py.py
+++ dags/py.py
@@ -19,43 +19,43 @@ def myfunc():
     print(res.status_code)
 
 with DAG('test-k8s-exec', start_date=days_ago(1), schedule_interval=None) as dag:
-    slack = SlackWebhookOperator(
-    http_conn_id=None,
-    task_id="slack-message",
-    webhook_token=os.environ["SLACK_TOKEN"],
-    message="asdf",
-    channel="#kubeflow-cron-alerts",
-    link_names=True,
-    executor_config={
-        "pod_override": k8s.V1Pod(
-            metadata=k8s.V1ObjectMeta(annotations={"allowlist": "hooks.slack.com"})
-        )
-    }
-    )
+    #slack = SlackWebhookOperator(
+    #http_conn_id=None,
+    #task_id="slack-message",
+    #webhook_token=os.environ["SLACK_TOKEN"],
+    #message="asdf",
+    #channel="#kubeflow-cron-alerts",
+    #link_names=True,
+    #executor_config={
+    #    "pod_override": k8s.V1Pod(
+    #        metadata=k8s.V1ObjectMeta(annotations={"allowlist": "hooks.slack.com"})
+    #    )
+    #}
+    #)
     
-    run_this = PythonOperator(
-    task_id='test',
-    python_callable=myfunc,
-    wait_for_downstream=False,
-    provide_context=True,
-    executor_config={
-        "pod_override": k8s.V1Pod(
-            metadata=k8s.V1ObjectMeta(annotations={"allowlist": "data.ssb.no,dm07-scan.adeo.no:1521"}),
-            spec=k8s.V1PodSpec(
-                containers=[
-                   k8s.V1Container(
-                      name="base",
-                      resources={
-                        "requests": {
-                            "cpu": "2"
-                        }
-                      }
-                   )
-                ]
-            )
-        )
-    },
-    dag=dag)
+    #run_this = PythonOperator(
+    #task_id='test',
+    #python_callable=myfunc,
+    #wait_for_downstream=False,
+    #provide_context=True,
+    #executor_config={
+    #    "pod_override": k8s.V1Pod(
+    #        metadata=k8s.V1ObjectMeta(annotations={"allowlist": "data.ssb.no,dm07-scan.adeo.no:1521"}),
+    #        spec=k8s.V1PodSpec(
+    #            containers=[
+    #               k8s.V1Container(
+    #                  name="base",
+    #                  resources={
+    #                    "requests": {
+    #                        "cpu": "2"
+    #                    }
+    #                  }
+    #               )
+    #            ]
+    #        )
+    #    )
+    #},
+    #dag=dag)
     
     then_this = KubernetesPodOperator(
         dag=dag,
@@ -65,4 +65,5 @@ with DAG('test-k8s-exec', start_date=days_ago(1), schedule_interval=None) as dag
         }
     )
 
-    slack >> run_this >> then_this
+    #slack >> run_this >> then_this
+    then_this
