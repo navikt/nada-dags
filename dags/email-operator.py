41773bab6a37e74a09e7d53da8d39fae994d8799
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-02-21 13:13:24 +0100

    Update email-operator.py

--- dags/email-operator.py
+++ dags/email-operator.py
@@ -6,7 +6,7 @@ with DAG(dag_id="epost", start_date=datetime(2023, 2, 21)) as dag:
     
     epost = EmailOperator(
         task_id="send-epost",
-        to=["Kyrre.Havik@nav.no"],
+        to=["Kyrre.Havik@nav.no","erik.vattekar@nav.no"],
         subject="Hei, fra Airflow",
         html_content="<h1>Hello world</h1><p>Dette er en e-post sendt fra en Airflow DAG</p>",
     )
