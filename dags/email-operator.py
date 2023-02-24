c38746f40207d61b564ef3e3eaec3a58771229cc
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-02-24 10:13:57 +0100

    Update email-operator.py

--- dags/email-operator.py
+++ dags/email-operator.py
@@ -3,15 +3,11 @@ from airflow.operators.email import EmailOperator
 from datetime import datetime
 from kubernetes import client as k8s
 
-with DAG(dag_id="epost", start_date=datetime(2023, 2, 21)) as dag:
+with DAG(dag_id="epost", start_date=datetime(2023, 2, 21), schedule_interval=None) as dag:
     
     epost = EmailOperator(
         task_id="send-epost",
-        to=["Kyrre.Havik@nav.no","erik.vattekar@nav.no"],
+        to=["erik.vattekar@nav.no"],
         subject="Hei, fra Airflow",
-        html_content="<h1>Hello world</h1><p>Dette er en e-post sendt fra en Airflow DAG</p>",
-        executor_config={
-        "pod_override": k8s.V1Pod(
-            metadata=k8s.V1ObjectMeta(annotations={"allowlist": "smtp.adeo.no:26"})
-        )}
+        html_content="<h1>Hello world</h1><p>Dette er en e-post sendt fra en Airflow DAG</p>"
     )
