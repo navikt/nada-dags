6aa666afbd106789651455f88dccce95cb0403e3
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-03-21 13:54:11 +0100

    Update bash-op-notebook.py

--- dags/bash-op-notebook.py
+++ dags/bash-op-notebook.py
@@ -8,20 +8,10 @@ from kubernetes import client as k8s
 
 with DAG('bash-operator-notebook', start_date=days_ago(1), schedule_interval=None, catchup=False) as dag:
 
-    t1 = BashOperator(
+    t1 = KubernetesPodOperator(
         task_id='notebook',
-        bash_command='papermill --log-output /dags/notebooks/mynb.ipynb /dags/output.ipynb',
-        executor_config={
-           "pod_override": k8s.V1Pod(
-               spec=k8s.V1PodSpec(
-                   containers=[
-                      k8s.V1Container(
-                         name="base",
-                         image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-notebooks:2023-03-08-d3684b7"
-                      )
-                   ]
-               )
-           )
-        },
+        cmds=["bash", "-c"],
+        arguments=["papermill", "--log-output", "/dags/notebooks/mynb.ipynb", "/dags/output.ipynb"],
+        image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-notebooks:2023-03-08-d3684b7",
         dag=dag
     )
