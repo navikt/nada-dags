00062d94ba9f2f45caad4c0f68f7f0bff84a7998
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-03-21 11:11:37 +0100

    Update bash-op-notebook.py

--- dags/bash-op-notebook.py
+++ dags/bash-op-notebook.py
@@ -5,8 +5,6 @@ from datetime import datetime
 
 with DAG('bash-operator-eksempel', start_date=datetime(2023, 2, 14), schedule_interval="*/5 * * * *", catchup=False) as dag:
 
-    os.environ["TEST"] = "hallo"
-
     t1 = BashOperator(
         task_id='notebook',
         bash_command='papermill --log-output ../notebooks/mynb.ipynb ../output.ipynb',
