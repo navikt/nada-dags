5600541835ec121add8b534b5f43e78af8c96259
Kyrre Havik <kyrremann@gmail.com>
2023-03-22 09:47:08 +0100

    fix: manglet import av k8s client

--- dags/papermill.py
+++ dags/papermill.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
+from kubernetes.client import models as k8s
 
 dockerImage = "europe-west1-docker.pkg.dev/knada-gcp/knada/papermill:2023-03-08-d3684b7"
 
