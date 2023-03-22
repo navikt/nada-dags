09936b3ba61e86fca276a01b64a732a93da96778
Kyrre Havik <kyrremann@gmail.com>
2023-03-22 12:18:02 +0100

    fix: fjernet schedule for dag

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 
-with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule_interval='*/5 * * * *', catchup=False) as dag:
+with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag:
 
     os.environ['WORLD'] = 'Mars'
 
