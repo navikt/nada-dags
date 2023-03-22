e6a01ba0fc0db705cf57edf902c816104032e2fe
Kyrre Havik <kyrremann@gmail.com>
2023-03-22 09:38:39 +0100

    fix: glemt import av pakker

--- dags/papermill.py
+++ dags/papermill.py
@@ -1,4 +1,6 @@
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
 
 dockerImage = "europe-west1-docker.pkg.dev/knada-gcp/knada/papermill:2023-03-08-d3684b7"
