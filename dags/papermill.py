1271910e47462efb5545367e79e997803d10620c
Kyrre Havik <kyrremann@gmail.com>
2023-03-22 12:31:35 +0100

    fix: test av papermill operator

--- dags/papermill.py
+++ dags/papermill.py
@@ -1,12 +1,17 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.papermill.operators.papermill import PapermillOperator
 from datetime import datetime
 from kubernetes.client import models as k8s
 
 dockerImage = 'europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-8018f16'
 
 with DAG('Papermill', start_date=datetime(2023, 3, 21), schedule_interval='0 10 * * *') as dag:
+
+    start = EmptyOperator()
+
     t1 = BashOperator(
         task_id='bashmill',
         bash_command='papermill --log-output /dags/notebooks/mynb.ipynb /dags/output.ipynb',
@@ -33,5 +38,12 @@ with DAG('Papermill', start_date=datetime(2023, 3, 21), schedule_interval='0 10 
             '/dags/output.ipynb'
         ]
     )
-    
-    t1 >> t2
+
+    t3 = PapermillOperator(
+        task_id="run_example_notebook",
+        input_nb="/dags/notebooks/mynb.ipynb",
+        output_nb="/dags/out-{{ execution_date }}.ipynb",
+        parameters={"msgs": "Ran from Airflow at {{ execution_date }}!"},
+    )
+
+    start = [t1, t2, t3]
