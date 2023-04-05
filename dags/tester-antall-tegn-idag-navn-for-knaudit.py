08008fa239ecd59192a0ae038dae7e522ffda8a0
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-05 10:31:46 +0200

    Update tester-antall-tegn-idag-navn-for-knaudit.py

--- dags/tester-antall-tegn-idag-navn-for-knaudit.py
+++ dags/tester-antall-tegn-idag-navn-for-knaudit.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 from kubernetes.client import models as k8s
 
-with DAG('foretak_iaweb_Informatica_kjerne_og_torg', start_date=datetime(2023, 3, 21), schedule=None) as dag:
+with DAG('foretak_iaweb_Informatica_kjerne_og_torg_laaaaaaaaaaaaaaaaaaaaaaaaaaaaaangtNavnForTesting123', start_date=datetime(2023, 3, 21), schedule=None) as dag:
 
     t1 = BashOperator(
         task_id='SYFRA_KJERNE_informatica_airflow',
