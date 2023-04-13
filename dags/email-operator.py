18069e30590b5868db8af2d2a46058974b53d772
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-13 12:35:13 +0200

    Update email-operator.py

--- dags/email-operator.py
+++ dags/email-operator.py
@@ -10,4 +10,9 @@ with DAG(dag_id="epost", start_date=datetime(2023, 2, 21), schedule_interval=Non
         to=["erik.vattekar@nav.no"],
         subject="Hei, fra Airflow",
         html_content="<h1>Hello world</h1><p>Dette er en e-post sendt fra en Airflow DAG</p>"
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "smtp.adeo.no:26"})
+            )
+        }
     )
