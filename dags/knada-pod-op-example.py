a95805a709a0f4689e2895d2ad944d03afcace01
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-19 12:52:12 +0200

    Update knada-pod-op-example.py

--- dags/knada-pod-op-example.py
+++ dags/knada-pod-op-example.py
@@ -15,6 +15,6 @@ with DAG(
               name="task",
               repo="navikt/nada-dags",
               branch="main",
-              script_path="notebooks/",
+              script_path="notebooks/script.py",
               image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow:2023-03-22-ffd1ef0"
   )
