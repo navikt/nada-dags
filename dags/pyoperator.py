b4315fc9abd6291d04aec161b38feac536a58c03
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-20 10:40:43 +0200

    Update pyoperator.py

--- dags/pyoperator.py
+++ dags/pyoperator.py
@@ -5,6 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from kubernetes import client as k8s
+from kode.modul import mycallable
 import os
 import logging
 import time
@@ -18,13 +19,16 @@ def myfunc():
 with DAG('pytho-operator', start_date=days_ago(1), schedule_interval=None) as dag:    
     run_this = PythonOperator(
     task_id='test-pythonoperator',
-    python_callable=myfunc,
+    python_callable=mycallable,
     executor_config={
         "pod_override": k8s.V1Pod(
             spec=k8s.V1PodSpec(
                 containers=[
                    k8s.V1Container(
                       name="base",
+                      env: [
+                        k8s.V1EnvVar("PYTHONPATH", "${PYTHONPATH}:/dags")
+                      ],
                       image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4"
                    )
                 ]
