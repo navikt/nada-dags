375b74da14a3131abb9c59cad0ef90373fea5c4a
Erik Vattekar <erik.vattekar@nav.no>
2023-04-20 15:20:59 +0200

    fjern kode mappe fra dette repoet

--- kode/modul.py
+++
@@ -1,9 +0,0 @@
-from kode.annenmodul import callme
-
-def mycallable():
-  import time
-  from google.cloud import bigquery
-  print("hallo fra en annen modul")
-  callme()
-  time.sleep(1000)
-  print("farvel")
