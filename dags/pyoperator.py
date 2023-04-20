e3abe11585240b397d2e3fbab959fe7c7072cd64
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-20 10:54:08 +0200

    Update pyoperator.py

--- dags/pyoperator.py
+++ dags/pyoperator.py
@@ -2,19 +2,9 @@ from airflow import DAG
 
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
-from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from kubernetes import client as k8s
 from kode.modul import mycallable
 import os
-import logging
-import time
-
-def myfunc():
-  import time
-  print("hello")
-  time.sleep(1000)
-  print("bye")
 
 with DAG('pytho-operator', start_date=days_ago(1), schedule_interval=None) as dag:    
     run_this = PythonOperator(
