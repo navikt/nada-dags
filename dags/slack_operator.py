82b45c5afdbd851dab9a0de1e03b903e52c90fdc
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-04-27 09:56:56 +0200

    Update slack_operator.py

--- dags/slack_operator.py
+++ dags/slack_operator.py
@@ -4,12 +4,10 @@ from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 import os
 
 with DAG('slack-operator', start_date=days_ago(1), schedule_interval=None) as dag:   
-  slack = SlackWebhookOperator(
-          task_id="airflow_task_failed",
-          http_conn_id=None,
-          webhook_token=os.environ["SLACK_TOKEN"],
-          message=f"@here Airflow task i DAG feilet",
-          channel="#kubeflow-cron-alerts",
-          link_names=True,
-          icon_emoji=":sadpanda:",
-      )
+  slack = SlackAPIPostOperator(
+    task_id="error",
+    dag=dag,
+    token=os.environ["SLACK_TOKEN"],
+    text="tester",
+    channel="#kubeflow-cron-alerts",
+  )
