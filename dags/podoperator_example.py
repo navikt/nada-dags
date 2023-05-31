165880c05c805bdd2b23047a158f837c8e864830
Erik Vattekar <erik.vattekar@nav.no>
2023-05-31 21:04:47 +0200

    switch order

--- dags/podoperator_example.py
+++ dags/podoperator_example.py
@@ -7,6 +7,17 @@ from kubernetes import client as k8s
 from common.podop_factory import create_pod_operator
 
 with DAG('pod-operator-examples', start_date=days_ago(1), schedule_interval=None) as dag:
+    podop_nb = create_pod_operator(
+        dag=dag,
+        name="nb_pod_op",
+        repo="navikt/nada-dags",
+        nb_path="etl/utdanning_kodeverk/insert_raa_nus_kodeverk_ssb/insert_raa_nus_kodeverk_ssb.ipynb",
+        slack_channel="#kubeflow-cron-alerts",
+        log_output=True,
+        retries=0,
+        delete_on_finish=False,
+    )
+    
     podop_script = create_pod_operator(
         dag=dag,
         name="python_pod_op",
@@ -18,15 +29,4 @@ with DAG('pod-operator-examples', start_date=days_ago(1), schedule_interval=None
         delete_on_finish=False,
     )
 
-    podop_nb = create_pod_operator(
-        dag=dag,
-        name="nb_pod_op",
-        repo="navikt/nada-dags",
-        nb_path="etl/utdanning_kodeverk/insert_raa_nus_kodeverk_ssb/insert_raa_nus_kodeverk_ssb.ipynb",
-        slack_channel="#kubeflow-cron-alerts",
-        log_output=True,
-        retries=0,
-        delete_on_finish=False,
-    )
-
-    podop_script >> podop_nb
+    podop_nb >> podop_script
