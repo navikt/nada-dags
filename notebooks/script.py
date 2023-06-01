e628e38c4018ecd50da0a26c0a8307c8a9f30248
Erik Vattekar <erik.vattekar@nav.no>
2023-06-01 10:28:43 +0200

    test xcom push

--- notebooks/script.py
+++ notebooks/script.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 print("hello")
 
@@ -10,3 +11,6 @@ print("fil:", data)
 res = requests.get("https://google.com")
 res.raise_for_status()
 print(res.text)
+
+with open('/airflow/xcom/return.json', 'w') as f:
+        f.write(json.dumps(data))
