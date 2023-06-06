32e6618a21192daf6eb56b980210bc1e7709734f
Erik Vattekar <erik.vattekar@nav.no>
2023-06-06 15:32:40 +0200

    asdf

--- dags/tester.py
+++ dags/tester.py
@@ -24,7 +24,7 @@ with DAG(dag_name, start_date=days_ago(1), schedule_interval=None) as dag:
         nb_path="test_notebook.ipynb",
         slack_channel="#kubeflow-cron-alerts",
         branch="main",
-        # log_output=True,
+        log_output=True,
         retries=0,
         do_xcom_push=True,
         # delete_on_finish=False,
