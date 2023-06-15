c4234907763f81bc58a1618cc8b917b47a12e6df
Erik Vattekar <erik.vattekar@nav.no>
2023-06-15 14:19:44 +0200

    asdf

--- dags/plotly.py
+++ dags/plotly.py
@@ -5,26 +5,26 @@ from airflow.operators.python_operator import PythonOperator
 from kubernetes import client as k8s
 import os
 
-from mymodule.mymodule import my_funky_func
+#from mymodule.mymodule import my_funky_func
 
 
-#def mycallable():
-#    import plotly
-#    print("hello")
+def mycallable():
+    import plotly
+    print("hello")
 
 
 
 with DAG('plotly', start_date=days_ago(1), schedule_interval=None) as dag:    
     run_this = PythonOperator(
     task_id='test-plotly',
-    python_callable=my_funky_func,
+    python_callable=mycallable,
     executor_config={
         "pod_override": k8s.V1Pod(
             spec=k8s.V1PodSpec(
                 containers=[
                    k8s.V1Container(
                       name="base",
-                      image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4"
+                      image="ghcr.io/navikt/airflow-pensjon-sb:v0"
                    )
                 ]
             )
