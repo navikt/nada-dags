fef4485c8b1fe437e84942e179243e73333da66a
Erik Vattekar <erik.vattekar@nav.no>
2023-06-23 13:10:00 +0200

    try absolute path to notebook file

--- dags/papermill.py
+++ dags/papermill.py
@@ -7,15 +7,15 @@ with DAG('Papermill', start_date=datetime(2023, 3, 21), schedule=None) as dag:
 
     t1 = BashOperator(
         task_id='bashmill',
-        bash_command='sleep 1000 && papermill --log-output mynb.ipynb output.ipynb',
+        bash_command='papermill --log-output /dags/notebooks/mynb.ipynb output.ipynb',
         executor_config={
            'pod_override': k8s.V1Pod(
                spec=k8s.V1PodSpec(
                    containers=[
                       k8s.V1Container(
-                         name='base',
-                         image='europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4',
-                         working_dir="/dags/notebooks"
+                          name='base',
+                          image='europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4',
+                          working_dir="/dags/notebooks"
                       )
                    ]
                )
