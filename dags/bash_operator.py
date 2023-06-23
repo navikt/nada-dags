983c09f48c1d44b35d4ca42a9c99431265947352
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-06-23 08:57:51 +0200

    image override test

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -11,6 +11,18 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag
     t1 = BashOperator(
         task_id='hello_task',
         bash_command='echo "Hello $WORLD at $(date)"'
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    init_containers=[
+                        k8s.V1Container(
+                            name="clone-code-repo",
+                            image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-06-22-9732e6c",
+                        )
+                    ]
+                )
+            )
+        )
     )
 
     t2 = BashOperator(
