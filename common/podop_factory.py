4080afbccbb3699f0ada29a7b226be7f6444a36c
Erik Vattekar <erik.vattekar@nav.no>
2023-09-22 13:51:15 +0200

    fjern debug

--- common/podop_factory.py
+++ common/podop_factory.py
@@ -170,7 +170,6 @@ def create_container_cmd(requirements_file, script_path, nb_path, quarto, log_ou
     elif quarto:
         try:
             command += f"cd {POD_WORKSPACE_DIR}/{Path(quarto['path']).parent} && quarto render {Path(quarto['path']).name} --to html --execute --output index.html -M self-contained:True &&" + \
-                        "ls -la &&" \
                         f"""curl -X PUT -F index.html=@index.html https://{quarto['environment']}/quarto/update/{quarto['id']} -H "Authorization:Bearer {quarto['token']}" """
         except KeyError:
             raise KeyError("path, environment, id and token must be provided for quarto")
