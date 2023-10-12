ea482b38f0c7be3997022bc409fff90ce7a68be6
Erik Vattekar <erik.vattekar@nav.no>
2023-10-12 08:07:24 +0200

    sjekk at begge deler fungerer

--- dags/slack_operator.py
+++ dags/slack_operator.py
@@ -30,3 +30,31 @@ with DAG('SlackOperator', start_date=days_ago(1), schedule_interval=None) as dag
       }
     ]
   )
+
+  slack_old = SlackAPIPostOperator(
+    task_id="error",
+    dag=dag,
+    token=os.environ["SLACK_TOKEN"],
+    text=":red_circle: tester",
+    channel="#kubeflow-cron-alerts",
+    attachments=[
+      {
+          "fallback": "min attachment",
+          "color": "#2eb886",
+          "pretext": "test",
+          "author_name": "Nada",
+          "title": "Nada",
+          "text": "test",
+          "fields": [
+              {
+                  "title": "Priority",
+                  "value": "High",
+                  "short": False
+              }
+          ],
+          "footer": "Nada"
+      }
+    ]
+  )
+
+  slack > slack_old
