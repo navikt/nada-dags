ebe9c213458c00c466752a241ceeea69f1263881
Erik Vattekar <erik.vattekar@nav.no>
2023-10-13 15:22:08 +0200

    test for new b dataverk-airflow

---
+++ dags/new_beginning.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from datetime import datetime
+from kubernetes.client import models as k8s
+from airflow.models import Variable
+from dataverk_airflow import notebook_operator, python_operator, quarto_operator
+
+
+with DAG('KubernetesPodOperator', start_date=datetime(2023, 2, 15), schedule=None) as dag:
+    nb_op = notebook_operator(
+        dag = dag,
+        name = "nb-op",
+        repo = "navikt/nada-dags",
+        nb_path = "notebooks/mynb.ipynb",
+    )
+
+    py_op = python_operator(
+        dag = dag,
+        name = "nb-op",
+        repo = "navikt/nada-dags",
+        script_path = "notebooks/script.py",
+    )
+
+    quarto_op = quarto_operator(
+        dag=dag,
+        name="",
+        repo="navikt/nada-dags",
+        quarto={
+            "path": "notebooks/quarto.ipynb",
+            "env": "dev",
+            "id": "bf48d8a4-05ca-47a5-a360-bc24171baf62",
+            "token": Variable.get("quarto_token"),
+        },
+    )
+
+    nb_op
+    py_op
+    quarto_op
--- notebooks/script.py
+++ notebooks/script.py
@@ -8,12 +8,3 @@ with open("/workspace/notebooks/minfil.json") as f:
     data = f.read()
 
 print("fil:", data)
-
-print(1/0)
-
-res = requests.get("https://google.com")
-res.raise_for_status()
-print(res.text)
-
-with open('/airflow/xcom/return.json', 'w') as f:
-        f.write(json.dumps(data))
