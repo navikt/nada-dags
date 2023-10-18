d4237c5a860bf165f8a8c8c974f9973ffc3fe9d0
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-10-18 14:47:08 +0200

    correct input params for deprecated dataverk airflow dag

--- dags/deprecated_dataverk_airflow.py
+++ dags/deprecated_dataverk_airflow.py
@@ -2,10 +2,9 @@ from airflow import DAG
 from dataverk_airflow.knada_operators import create_knada_nb_pod_operator
 from airflow.utils.dates import days_ago
 
-with DAG('dag', start_date=days_ago(1), schedule_interval=None) as dag:
+with DAG('DeprecatedDataverkAirflow', start_date=days_ago(1), schedule_interval=None) as dag:
     task = create_knada_nb_pod_operator(dag=dag,
                                         name="knada-pod-operator",
-                                        repo="navikt/repo",
+                                        repo="navikt/nada-dags",
                                         nb_path="notebooks/mynb.ipynb",
-                                        retries=1,
                                         branch="main")
