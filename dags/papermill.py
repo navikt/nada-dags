f28b017aeb2b36bba1cc9f5aa47c5427b3342b97
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-10-18 15:28:26 +0200

    fix

--- dags/papermill.py
+++ dags/papermill.py
@@ -14,7 +14,7 @@ with DAG('Papermill', start_date=datetime(2023, 3, 21), schedule=None) as dag:
                    containers=[
                       k8s.V1Container(
                           name='base',
-                          image='europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4'
+                          image='europe-north1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-08-31-ab6e198'
                       )
                    ]
                )
