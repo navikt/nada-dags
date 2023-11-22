f9295ec9483433e2866bb0a50ac05ec028e5954d
Erik Vattekar <erik.vattekar@nav.no>
2023-11-22 14:40:37 +0100

    test impersonation with gcs -> bq operator

--- dags/oracle_to_bigquery.py
+++ dags/oracle_to_bigquery.py
@@ -17,15 +17,16 @@ with DAG('OracleToBigqueryOperator', start_date=datetime(2023, 2, 14), schedule=
         export_format="csv"
     )
 
-    # t2 = GCSToBigQueryOperator(
-    #     task_id="write-to-bq",
-    #     bucket="airflow-oracle-to-bq",
-    #     gcp_conn_id="google_con",
-    #     destination_project_dataset_table="nada-dev-db2e.test.fra_oracle",
-    #     autodetect=True,
-    #     write_disposition="WRITE_TRUNCATE",
-    #     source_objects="dump",
-    #     source_format="csv"
-    # )
+    t2 = GCSToBigQueryOperator(
+        task_id="write-to-bq",
+        bucket="airflow-oracle-to-bq",
+        gcp_conn_id="google_con_without_json_key",
+        destination_project_dataset_table="nada-dev-db2e.test.fra_oracle",
+        impersonation_chain="knada-hyka@knada-gcp.iam.gserviceaccount.com",
+        autodetect=True,
+        write_disposition="WRITE_TRUNCATE",
+        source_objects="dump",
+        source_format="csv"
+    )
 
-    t1 #>> t2
+    t1 >> t2
