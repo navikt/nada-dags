3a029dda1046e804779584c077149d264d7e5ea4
Erik Vattekar <erik.vattekar@nav.no>
2023-11-22 13:47:58 +0100

    test impersonation_chain

--- dags/oracle_to_bigquery.py
+++ dags/oracle_to_bigquery.py
@@ -6,25 +6,26 @@ from datetime import datetime
 
 
 with DAG('OracleToBigqueryOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag:
-    # t1 = OracleToGCSOperator(
-    #     task_id="write-to-bucket",
-    #     oracle_conn_id="oracle_con",
-    #     gcp_conn_id="google_con",
-    #     sql="SELECT * FROM nada",
-    #     bucket="airflow-oracle-to-bq",
-    #     filename="dump",
-    #     export_format="csv"
-    # )
-
-    t2 = GCSToBigQueryOperator(
-        task_id="write-to-bq",
+    t1 = OracleToGCSOperator(
+        task_id="write-to-bucket",
+        oracle_conn_id="oracle_con",
+        #gcp_conn_id="google_con",
+        impersonation_chain="knada-hyka@knada-gcp.iam.gserviceaccount.com"
+        sql="SELECT * FROM nada",
         bucket="airflow-oracle-to-bq",
-        gcp_conn_id="google_con",
-        destination_project_dataset_table="nada-dev-db2e.test.fra_oracle",
-        autodetect=True,
-        write_disposition="WRITE_TRUNCATE",
-        source_objects="dump",
-        source_format="csv"
+        filename="dump",
+        export_format="csv"
     )
 
-    t2
+    # t2 = GCSToBigQueryOperator(
+    #     task_id="write-to-bq",
+    #     bucket="airflow-oracle-to-bq",
+    #     gcp_conn_id="google_con",
+    #     destination_project_dataset_table="nada-dev-db2e.test.fra_oracle",
+    #     autodetect=True,
+    #     write_disposition="WRITE_TRUNCATE",
+    #     source_objects="dump",
+    #     source_format="csv"
+    # )
+
+    t1 #>> t2
