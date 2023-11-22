c502fb4a4a5d3f86b6a767c5edd37e5f5fc8a316
Erik Vattekar <erik.vattekar@nav.no>
2023-11-22 12:33:59 +0100

    add test dag for oracle -> bq via gcs

---
+++ dags/oracle_to_bigquery.py
@@ -0,0 +1,15 @@
+import os
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.oracle_to_gcs import OracleToGCSOperator
+from datetime import datetime
+
+
+with DAG('OracleToBigqueryOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag:
+    t1 = OracleToGCSOperator(
+        task_id='write-to-bucket',
+        oracle_conn_id="oracle_con",
+        sql="SELECT * FROM nada",
+        bucket="airflow-oracle-to-bq",
+        filename="dump",
+        export_format="csv"
+    )
