6de47063add93db0912267c14c5d53c5986dc5d5
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-11-23 10:46:48 +0100

    add allowlist to slack operator example

--- dags/slack_operator.py
+++ dags/slack_operator.py
@@ -7,6 +7,11 @@ with DAG('SlackOperator', start_date=days_ago(1), schedule_interval=None) as dag
   slack = SlackAPIPostOperator(
     task_id="error",
     dag=dag,
+    executor_config={
+      "pod_override": k8s.V1Pod(
+          metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
+      )
+    }
     slack_conn_id="slack_connection",
     text=":red_circle: tester",
     channel="#kubeflow-cron-alerts",
