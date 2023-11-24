2d04327a8a68f3863070d47b27fbd4ca6452844c
Kyrre Havik <Kyrremann@gmail.com>
2023-11-24 15:22:30 +0100

    feat: enkel slack test, sender melding etter 60s

---
+++ dags/simple_slack_test.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.slack.notifications.slack import send_slack_notification
+from kubernetes import client as k8s
+import time
+
+
+def mycallable():
+    time.Sleep(60)
+
+
+with DAG("PythonOperator", start_date=days_ago(1), schedule_interval=None) as dag:
+    run_this = PythonOperator(
+        task_id="test-pythonoperator",
+        on_success_callback=[
+            send_slack_notification(
+                text="The DAG {{ dag.dag_id }} succeeded",
+                channel="#nada-test",
+                slack_conn_id="slack_connection",
+                username="Airflow",
+            )
+        ],
+        on_failure_callback=[
+            send_slack_notification(
+                text="The task {{ ti.task_id }} failed",
+                channel="#nada-test",
+                slack_conn_id="slack_connection",
+                username="Airflow",
+            )
+        ],
+        python_callable=mycallable,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "hooks.slack.com"})
+            )
+        },
+        dag=dag,
+    )
