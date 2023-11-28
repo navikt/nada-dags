6dce2b374a99f292d609672ae82aa6ff00f1bd93
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-11-28 11:10:11 +0100

    test at scan adresser legges til

--- dags/quarto_operator.py
+++ dags/quarto_operator.py
@@ -16,5 +16,5 @@ with DAG('Quarto', start_date=days_ago(1), schedule=None) as dag:
             "token": Variable.get("TEAM_TOKEN"),
         },
         requirements_path="notebooks/requirements.txt",
-        allowlist=["datamarkedsplassen.intern.nav.no"],
+        allowlist=["datamarkedsplassen.intern.nav.no","dm08-scan.adeo.no:1521"],
     )
