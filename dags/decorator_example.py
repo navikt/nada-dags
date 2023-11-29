5c22f6bea028c1b6d6f9df51b9dadf6ca0112443
Erik Vattekar <erik.vattekar@nav.no>
2023-11-29 07:40:58 +0100

    test pod override for decorator task in dag

---
+++ dags/decorator_example.py
@@ -0,0 +1,17 @@
+import os
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+from kubernetes import client as k8s
+
+
+with DAG('DecoratorExampleWithPodOverride', start_date=datetime(2023, 2, 14), schedule=None) as dag:
+    @task(
+        executor_config = {
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
+            )
+        }
+    )
+    def myfunc(value: str):
+        print(value)
