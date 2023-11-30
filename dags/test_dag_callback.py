1c01213ae418aa830af424521326a5c680bd64b9
Erik Vattekar <erik.vattekar@nav.no>
2023-11-30 09:35:58 +0100

    test on success callback posting slack with allowlist

---
+++ dags/test_dag_callback.py
@@ -0,0 +1,32 @@
+import os
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+from datetime import datetime
+from kubernetes import client as k8s
+
+def slack_success():
+  SlackAPIPostOperator(
+    task_id="slack-success",
+    slack_conn_id="slack_connection",
+    text="suksess",
+    channel="#kubeflow-cron-alerts",
+    executor_config = {
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com,hooks.slack.com"})
+            )
+        }
+  ).execute()
+
+with DAG('BashOperator', 
+        start_date=datetime(2023, 2, 14), 
+        default_args={'on_success_callback': slack_success},
+        schedule=None
+) as dag:
+
+    t1 = BashOperator(
+        task_id='hello_task',
+        bash_command='echo "Hello $(date)"'
+    )
+    
+    t1
