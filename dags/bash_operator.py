f024e81da8e359946202c6e796851c731c1b7c2d
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2023-12-19 11:15:22 +0100

    Update bash_operator.py

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -11,6 +11,11 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag
     t1 = BashOperator(
         task_id='hello_task',
         bash_command='echo "Hello $WORLD at $(date)"'
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "35.235.240.1:89"})
+            )
+        }
     )
 
     t2 = BashOperator(
