17fc242c0d4141fabac809ead4a2a154b1bf59d0
Erik Vattekar <erik.vattekar@nav.no>
2024-01-11 15:11:47 +0100

    add debug sleep

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -11,7 +11,7 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag
 
     t1 = BashOperator(
         task_id='hello_task',
-        bash_command='echo "Hello $WORLD at $(date)"',
+        bash_command='echo "Hello $WORLD at $(date); sleep 150"',
         executor_config={
             "pod_override": k8s.V1Pod(
                 metadata=k8s.V1ObjectMeta(annotations={"allowlist": "35.235.240.1:89"})
