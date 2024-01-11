d9e3c903700801b9bb9127747d4746442a117568
Erik Vattekar <erik.vattekar@nav.no>
2024-01-11 13:40:18 +0100

    add quarto book dag

---
+++ dags/quarto_book.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from dataverk_airflow import quarto_operator
+
+
+with DAG('QuartoBook', start_date=days_ago(1), schedule=None) as dag:
+    quarto_op = quarto_operator(
+        dag=dag,
+        name="quarto-op",
+        repo="navikt/nada-dags",
+        quarto={
+            "folder": "notebooks/quartobook",
+            "env": "dev",
+            "id": "757da08e-031e-4fac-a5f0-fffe6d2d96b6",
+            "token": Variable.get("TEAM_TOKEN"),
+        },
+        requirements_path="notebooks/requirements.txt",
+        allowlist=["datamarkedsplassen.intern.dev.nav.no"],
+    )
