e991e11d3d0a5c05805f2ae05d72e595802db582
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2024-04-22 15:43:11 +0200

    Update bash_operator.py

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -15,6 +15,14 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag
         executor_config={
             "pod_override": k8s.V1Pod(
                 metadata=k8s.V1ObjectMeta(annotations={"allowlist": "35.235.240.1:89"})
+                spec=k8s.V1PodSpec(
+                containers=[
+                   k8s.V1Container(
+                      name="base",
+                      image="europe-north1-docker.pkg.dev/nais-management-233d/nada/dakan-api-digdir:2024.03.25-13.21-347d0e8",
+                   )
+                ]
+            )
             )
         }
     )
