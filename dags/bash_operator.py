da202b1b3e8db80c2d5fd3f704a902fba929903e
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2024-05-15 09:33:27 +0200

    remove custom image bashoperator

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -11,20 +11,7 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag
 
     t1 = BashOperator(
         task_id='hello_task',
-        bash_command='echo "Hello $WORLD at $(date)"',
-        executor_config={
-            "pod_override": k8s.V1Pod(
-                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "35.235.240.1:89"}),
-                spec=k8s.V1PodSpec(
-                containers=[
-                   k8s.V1Container(
-                      name="base",
-                      image="europe-north1-docker.pkg.dev/nais-management-233d/dv-a-team/dvh-images-airflow-dbt:2024.04.11-08.08-76c2dff",
-                   )
-                ]
-            )
-            )
-        }
+        bash_command='echo "Hello $WORLD at $(date)"'
     )
 
     t2 = BashOperator(
