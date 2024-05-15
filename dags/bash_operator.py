3b194a54501ac74a63a59fc0ee0678cb6a422122
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2024-05-15 11:00:51 +0200

    Update bash_operator.py

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -5,8 +5,10 @@ from datetime import datetime
 from kubernetes import client as k8s
 import logging
 
-log: logging.log = logging.getLogger("airflow.task")
-log.setLevel(logging.INFO)
+log = logging.getLogger("airflow.task")
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+log.addHandler(handler)
 
 with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule=None) as dag:
 
