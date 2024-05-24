05733f69a2837dbe5173a8a988e5c2e58846e6f5
Erik Vattekar <erik.vattekar@nav.no>
2024-05-24 11:05:36 +0200

    add schedule for cloudsql example

--- dags/read_postgres.py
+++ dags/read_postgres.py
@@ -4,7 +4,7 @@ from dataverk_airflow import python_operator
 
 
 # For å konfigurere tilgang for airflow service account, se https://docs.knada.io/analyse/eksempler/#cloud-sql-iam-database-authentication
-with DAG('CloudSQLPostgres', start_date=days_ago(1), schedule=None) as dag:
+with DAG('CloudSQLPostgres', start_date=days_ago(1), schedule="50 8 * * 1-5", catchup=False) as dag:
     postgres_op = python_operator(
         dag=dag,
         name="postgres-op",
