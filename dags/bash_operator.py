3a1a02bd78584929994dda4961f569745b237d64
Erik Vattekar <erik.vattekar@nav.no>
2024-05-24 12:34:32 +0200

    test alert for bashoperator

--- dags/bash_operator.py
+++ dags/bash_operator.py
@@ -1,10 +1,8 @@
 import os
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.providers.slack.notifications.slack import send_slack_notification
 from datetime import datetime
-from kubernetes import client as k8s
-import logging
-import sys
 
 with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule="0 8 * * 1-5", catchup=False) as dag:
 
@@ -12,7 +10,23 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule="0 8 * * 1-5
 
     t1 = BashOperator(
         task_id='hello_task',
-        bash_command='echo "Hello Earth"'
+        bash_command='echo "Hello Earth"',
+        on_success_callback=[
+            send_slack_notification(
+                text="The DAG {{ run_id }} succeeded",
+                channel="#nada-alerts-dev",
+                slack_conn_id="slack_connection",
+                username="Airflow",
+            )
+        ],
+        on_failure_callback=[
+            send_slack_notification(
+                text="The DAG {{ run_id }} failed",
+                channel="#nada-alerts-dev",
+                slack_conn_id="slack_connection",
+                username="Airflow",
+            )
+        ],
     )
 
     t2 = BashOperator(
@@ -20,6 +34,15 @@ with DAG('BashOperator', start_date=datetime(2023, 2, 14), schedule="0 8 * * 1-5
         bash_command='echo "Bye $WORLD"',
         env={
             'WORLD': 'Earth'
-        })
+        },
+        on_failure_callback=[
+            send_slack_notification(
+                text="The DAG {{ run_id }} failed",
+                channel="#nada-alerts-dev",
+                slack_conn_id="slack_connection",
+                username="Airflow",
+            )
+        ]
+        )
 
     t1 >> t2
