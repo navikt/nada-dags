16be15e265522227aebe767cce9961906ad1dd6a
Erik Vattekar <erik.vattekar@nav.no>
2024-11-22 14:06:58 +0100

    resolve merge conflict

--- dags/dataverk_airflow_notebook_operator.py
+++ dags/dataverk_airflow_notebook_operator.py
@@ -10,6 +10,5 @@ with DAG('DataverkAirflowNotebook', start_date=days_ago(1), schedule="0 8 * * 1-
         repo="navikt/nada-dags",
         nb_path="notebooks/mynb.ipynb",
         requirements_path="notebooks/requirements.txt",
-        slack_channel="#kubeflow-cron-alerts",
-        retries=0,
+        slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
     )
