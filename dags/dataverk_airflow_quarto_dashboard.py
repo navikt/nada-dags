53a6948abee0fd4fd4fa80167d68d7470ce0ac97
Erik Vattekar <erik.vattekar@nav.no>
2025-03-18 15:33:50 +0100

    chore(dags): add quarto dashboard example

---
+++ dags/dataverk_airflow_quarto_dashboard.py
@@ -0,0 +1,26 @@
+import os
+from airflow import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from dataverk_airflow import quarto_operator
+
+
+dmp_host = Variable.get('MARKEDSPLASSEN_HOST_DEV', default_var=None)
+if dmp_host:
+    os.environ["MARKEDSPLASSEN_HOST"] = dmp_host
+
+
+with DAG('DataverkAirflowQuartoDashboard', start_date=days_ago(1), schedule="5 12 * * 1-5", catchup=False) as dag:
+    quarto_op = quarto_operator(
+        dag=dag,
+        name="quarto-op",
+        repo="navikt/nada-dags",
+        quarto={
+            "folder": "notebooks/quarto",
+            "env": "dev",
+            "id": "f87e918a-e5bb-4549-aba2-78ebc6c6ae2c",
+            "token": Variable.get("NADA_TOKEN_DEV"),
+        },
+        requirements_path="notebooks/requirements.txt",
+        slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
+    )
