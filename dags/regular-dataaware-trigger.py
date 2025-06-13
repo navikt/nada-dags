31cf734c8e04f94d1fb61dd92969339eb5b4d906
Erik Vattekar <erik.vattekar@nav.no>
2025-06-13 12:35:25 +0200

    test no image override

--- dags/regular-dataaware-trigger.py
+++ dags/regular-dataaware-trigger.py
@@ -14,18 +14,18 @@ from airflow.decorators import dag, task
 )
 def trigger_test():
     @task(
-        executor_config={
-            "pod_override": k8s.V1Pod(
-                spec=k8s.V1PodSpec(
-                    containers=[
-                        k8s.V1Container(
-                            name="base",
-                            image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow@sha256:85e5787aaaf694379fb031954cb53a04a80fcc08934991fcb90fae65102d5fe3",
-                        )
-                    ]
-                ),
-            )
-        },
+        # executor_config={
+        #     "pod_override": k8s.V1Pod(
+        #         spec=k8s.V1PodSpec(
+        #             containers=[
+        #                 k8s.V1Container(
+        #                     name="base",
+        #                     image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow@sha256:85e5787aaaf694379fb031954cb53a04a80fcc08934991fcb90fae65102d5fe3",
+        #                 )
+        #             ]
+        #         ),
+        #     )
+        # },
     )
     def transfer():
         print("triggered by data aware scheduling")
