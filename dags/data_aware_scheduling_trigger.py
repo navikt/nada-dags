1578b6771b9b216d205ca76748124ea6911e626b
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2025-06-13 12:10:01 +0200

    tester image med gammel airflow versjon

--- dags/data_aware_scheduling_trigger.py
+++ dags/data_aware_scheduling_trigger.py
@@ -1,6 +1,7 @@
 from airflow import DAG, Dataset
 from airflow.utils.dates import days_ago
 from dataverk_airflow import python_operator
+import kubernetes as k8s
 
 
 with DAG('DataAwareSchedulingTrigger', start_date=days_ago(1), schedule=[Dataset("gs://local-flyte-test/file.txt")], catchup=False) as dag:
@@ -12,4 +13,16 @@ with DAG('DataAwareSchedulingTrigger', start_date=days_ago(1), schedule=[Dataset
         requirements_path="notebooks/requirements_write_to_bucket.txt",
         retries=0,
         slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow@sha256:85e5787aaaf694379fb031954cb53a04a80fcc08934991fcb90fae65102d5fe3",
+                        )
+                    ]
+                ),
+            )
+        }
     )
