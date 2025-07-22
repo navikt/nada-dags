8a870702a28b6a174665a17e05891b08c4bd6c0a
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2025-07-22 17:10:40 +0200

    trigge init error

--- dags/dataverk_airflow_python_operator.py
+++ dags/dataverk_airflow_python_operator.py
@@ -8,7 +8,7 @@ with DAG('DataverkAirflowPython', start_date=days_ago(1), schedule="10 8 * * 1-5
         dag=dag,
         name="python-op",
         repo="navikt/nada-dags",
-        branch="dev",
+        branch="finnes-ikke",
         delete_on_finish=False,
         script_path="notebooks/script.py",
         requirements_path="notebooks/requirements.txt",
