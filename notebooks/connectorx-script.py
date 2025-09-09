10bb6bfb18bac2456aadb5f0e20557fb7b3816bc
Erik Vattekar <erik.vattekar@nav.no>
2025-09-09 09:33:09 +0200

    allow double quoted strings for formatted string

--- notebooks/connectorx-script.py
+++ notebooks/connectorx-script.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import connectorx as cx
 
-conn_string = f"oracle://{os.environ["ORACLE_DB_USER"]}:{os.environ["ORACLE_DB_PASSWORD"]}@{os.environ["ORACLE_DB_HOST"]}:{os.environ["ORACLE_DB_PORT"]}/?service_name={os.environ["ORACLE_DB_SERVICE_NAME"]}"
+conn_string = f"""oracle://{os.environ["ORACLE_DB_USER"]}:{os.environ["ORACLE_DB_PASSWORD"]}@{os.environ["ORACLE_DB_HOST"]}:{os.environ["ORACLE_DB_PORT"]}/?service_name={os.environ["ORACLE_DB_SERVICE_NAME"]}"""
 res = cx.read_sql(conn_string, f"SELECT * FROM {os.environ['ORACLE_DB_TABLE_NAME']}")
 
 print("Fetched data using connectorx:")
