601ccda916165e4c4e0c2604aee8c8d50f329944
Erik Vattekar <erik.vattekar@nav.no>
2025-09-10 14:38:18 +0200

    add dataverk_airflow_custom_entrypoint dag

---
+++ dags/dataverk_airflow_custom_entrypoint.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from dataverk_airflow import kubernetes_operator
+
+
+with DAG('DataverkAirflowPythonEntrypointOverride', start_date=days_ago(1), schedule="10 13 * * 1-5", catchup=False) as dag:
+    py_op = kubernetes_operator(
+        dag=dag,
+        name="dataverk-airflow-entrypoint-override",
+        repo="navikt/nada-dags",
+        image="europe-north1-docker.pkg.dev/knada-gcp/knada-north/dataverk-airflow-python-3.11-man:v126",
+        entrypoint=[
+            "dbt",
+            "run",
+        ],
+        retries=0,
+        slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
+    )
