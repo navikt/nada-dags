aa0209b4a31fa36f6c80130d0dce27b9a04dbb07
Erik Vattekar <erik.vattekar@nav.no>
2025-09-10 21:05:25 +0200

    add entrypoint override example

---
+++ dags/dataverk_airflow_custom_entrypoint.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from dataverk_airflow import kubernetes_operator
+
+
+with DAG('DataverkAirflowPythonEntrypointOverride', start_date=days_ago(1), schedule="10 13 * * 1-5", catchup=False) as dag:
+    py_op = kubernetes_operator(
+        dag=dag,
+        name="dataverk-airflow-entrypoint-override",
+        repo="navikt/nada-dags",
+        image="europe-north1-docker.pkg.dev/knada-gcp/knada-north/dataverk-airflow-python-3.11-man:v128",
+        entrypoint=[
+            "dbt",
+            "run",
+            "--project-dir",
+            "/workspace/notebooks/dbt/nada_dbt_test",
+            "--profiles-dir",
+            "/workspace/notebooks/dbt/nada_dbt_test",
+            "--profile",
+            "nada_dbt_test",
+        ],
+        retries=0,
+        slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
+    )
