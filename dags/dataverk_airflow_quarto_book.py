e5ee1ed6a9db51ae6500dd3e134d709ed027cce9
Erik Vattekar <erik.vattekar@nav.no>
2025-10-13 13:17:29 +0200

    use default image

--- dags/dataverk_airflow_quarto_book.py
+++ dags/dataverk_airflow_quarto_book.py
@@ -16,7 +16,6 @@ with DAG('DataverkAirflowQuartoBook', start_date=days_ago(1), schedule="15 8 * *
         name="quarto-op",
         repo="navikt/nada-dags",
         python_version="3.10",
-        image="europe-north1-docker.pkg.dev/knada-gcp/knada-north/dv-airflow:129",
         use_uv_pip_install=True,
         quarto={
             "folder": "notebooks/quartobook",
