04c0c8cb0d88db7ad6e72f1ecd9fedcee75a751d
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2026-02-02 15:30:40 +0100

    Add Kubernetes resource requirements to Python operator
    Added Kubernetes resource requirements for the Python operator.

--- dags/dataverk_airflow_python_operator.py
+++ dags/dataverk_airflow_python_operator.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from dataverk_airflow import python_operator
+from kubernetes import client
 
 
 with DAG('DataverkAirflowPython', start_date=days_ago(1), schedule="10 8 * * 1-5", catchup=False) as dag:
@@ -13,5 +14,8 @@ with DAG('DataverkAirflowPython', start_date=days_ago(1), schedule="10 8 * * 1-5
         retries=0,
         do_xcom_push=True,
         slack_channel="{{ var.value.get('SLACK_ALERT_CHANNEL') }}",
-   
+        client.V1ResourceRequirements(
+            requests={"memory": "1G", "cpu": "5",},
+            limits={"memory": "1G"}
+        )
     )
